151ccafbfd80986b1182e4f6af83f1bbeefed457
Christopher <christopher.hsc2520@gmail.com>
2025-01-23 21:35:32 +0100

    more general styling + responsive design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -10,8 +10,24 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   activeLink: string = '';
+  isMenuOpen: boolean = false;
 
+  // set toggled link active with styling
   setActive(link: string): void {
     this.activeLink = link;
+
+    // Close the menu after 2 seconds on mobile
+    if (this.isMenuOpen) {
+      setTimeout(() => {
+        this.isMenuOpen = false;
+        console.log('Menu closed after clicking:', link);
+      }, 1200);
+    }
+  }
+
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log("is menu open?", this.isMenuOpen)
   }
 }
